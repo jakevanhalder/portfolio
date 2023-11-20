8aef66aa62e9482324b4bcbf8e7a55c11af88460
jakevanhalder <124746901+jakevanhalder@users.noreply.github.com>
2023-11-20 01:09:24 -0800

    finished the mobile navigation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const primaryHeader = document.querySelector('.primary-header');
+const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
